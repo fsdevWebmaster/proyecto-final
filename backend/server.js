fe0862985be022bb6667ae4fc4af0e2f984c1e2a
fis48 <info@fidelsilva.com>
2023-07-27 19:31:09 -0500

    - Local mongo uri pointed to PFM database. - Api port added. - Auth secret added. - Connection to mongo added in server file. - Register and Login endpoints implemented. - Require expressions removed due to unify the use of modules to ES. - Unify User model with the openApi documentation. fixes #17

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes/app.routes.js';
+import { connectDB } from './database/config.js';
 dotenv.config();
 const app = express();
 
@@ -14,7 +15,13 @@ app.use(express.json());
 // routes
 app.use('/api', routes);
 
+connectDB()
+  .then((result) => {
+    console.log("Mongo db connected.");
+  }).catch((err) => {
+    console.log("Error connection to mongo db.");
+  });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Api server listening at :${process.env.PORT}`);
+app.listen(process.env.LOCAL_PORT, () => {
+  console.log(`Api server listening at :${process.env.LOCAL_PORT}`);
 }); 
