fbcb3c364be7a8f6a94df635d8df48b8f2548882
FranJimenezQ <flakundo@gmail.com>
2023-08-05 18:42:01 -0600

    herencia de modelos en journey

--- backend/models/journey.model.js
+++ backend/models/journey.model.js
@@ -1,17 +1,22 @@
 // Journey.js
 
 import mongoose from 'mongoose';
+const extendSchema = require('mongoose-extend-schema');
 
+//Revisar la importacion de extendSchema y la funcionalidad. heredar los atributos de los otros modelos 
+const journeySchema = extendSchema(containerSchema, {
 
-const journeySchema = new mongoose.Schema({
-  journeyId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+    journeyId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    date:{
+      type: date,
+      required: true
+    }
+} );
 
-});
-
-const Container = mongoose.model('Journey', journeySchema);
+const Journey = mongoose.model('Journey', journeySchema);
 
 export default Journey;
