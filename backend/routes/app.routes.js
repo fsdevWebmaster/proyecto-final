dcec3e54f3d5f2a050871cbc570cfddee09e35af
fis48 <info@fidelsilva.com>
2023-08-09 14:05:47 -0500

    - Auth middleware implemented and added to router. fixes #9

--- backend/routes/app.routes.js
+++ backend/routes/app.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { register, login, profile } from "../controllers/users.controller.js";
+import { userAuth } from "../middlewares/users.middleware.js";
 const router = express.Router();
 
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/profile', profile);
+router.post('/profile', userAuth, profile);
 
 export default router;
 
