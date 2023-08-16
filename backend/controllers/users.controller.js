c9ad1900c3d418a3af19972c1b87c389318220a3
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-08-15 20:20:06 -0500

    - Get by id, create, update and search controllers implemented. (#50)
    - Get by id, create, update and search endpoints implemented.
    
    Co-authored-by: fis48 <info@fidelsilva.com>

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
-
+import Driver from '../models/driver.model.js';
 
 export const register = (req, res) => {
   let { body } = req;
@@ -91,3 +91,67 @@ export const profile = (req, res) => {
     });
 }
 
+export const newDriver = (req, res) => {
+  const newDriver = new Driver(req.body)  
+  newDriver.save()
+    .then((result) => {
+      return res.json(result)
+    }).catch((err) => {
+      if (err.message.includes('E11000 duplicate key error')) {
+        return res.status(400).json({ error: 'User already registered.' })
+      }
+      if (err.message.includes('is required')) {
+        return res.status(400).json({ error: 'Wrong or missing data.' })
+      }
+    });
+}
+
+export const getDriver = (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ error: "Wrong or missing id." })
+  }
+
+  Driver.findById(id)
+    .then((result) => {
+      return res.json(result)
+    }).catch((err) => {
+      return res.status(400).json({ error: err.message })
+    });
+}
+
+export const searchDriver = (req, res) => {
+  const { idDoc } = req.params;
+  if (!idDoc) {
+    return res.status(500).json({ TODO: "Search error" })
+  }
+  Driver.find({ idDoc })
+    .then((result) => {
+      if (result.length === 0) {
+        return res.status(404).json({ error: "User not found." })
+      }
+      return res.json(result[0])
+    }).catch((err) => {
+      return res.status(500).json({ TODO: "Error finding driver by idDoc:", err })
+    });
+
+}
+
+export const updateDriver = (req, res) => {
+  const { id } = req.params
+  const { name, idDoc, email } = req.body
+  Driver.findById(id)
+    .then((result) => {
+      result.name = name
+      result.idDoc = idDoc
+      result.email = email
+      result.save()
+        .then((saveResult) => {
+          return res.json(saveResult)
+        }).catch((saveErr) => {
+          return res.status(500).json({ TODO: saveErr })
+        });
+    }).catch((err) => {
+      
+    });
+}
--- backend/routes/app.routes.js
+++ backend/routes/app.routes.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { register, login, profile } from "../controllers/users.controller.js";
+import { 
+  register, 
+  login, 
+  profile, 
+  newDriver, 
+  getDriver, 
+  searchDriver,
+  updateDriver 
+} from "../controllers/users.controller.js";
 import { userAuth } from "../middlewares/users.middleware.js";
 const router = express.Router();
 
@@ -7,6 +15,13 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/profile', userAuth, profile);
+
+router.post('/driver', userAuth, newDriver);
+router.get('/driver/:id', userAuth, getDriver);
+router.get('/search-driver/:idDoc', userAuth, searchDriver);
+router.patch('/driver/:id', userAuth, updateDriver);
+
+
 
 export default router;
 
