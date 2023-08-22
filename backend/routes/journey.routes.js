581cc8aecf01082cacda8a4e380a513dae93d3fb
fis48 <info@fidelsilva.com>
2023-08-22 15:14:09 -0500

    - Status set to on hold when journey is created. - Get in transit journeys implemented.

--- backend/routes/journey.routes.js
+++ backend/routes/journey.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const journeyRouter = express.Router();
 import { userAuth } from "../middlewares/users.middleware.js";
-import { newContainer, newJourney } from "../controllers/journey.controller.js";
+import { getInTransit, newContainer, newJourney } from "../controllers/journey.controller.js";
 
 journeyRouter.post('/container', userAuth, newContainer)
 journeyRouter.post('/journey', userAuth, newJourney);
+journeyRouter.get('/in-transit', userAuth, getInTransit);
 
 export default journeyRouter;
