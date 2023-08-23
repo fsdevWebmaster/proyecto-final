6bcff6a667e68b2f74b6dbc467a4a9cb77f52746
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-08-22 20:08:02 -0500

    - Container routes and controller added. (#66)
    - Container model response formatted.
    
    Co-authored-by: fis48 <info@fidelsilva.com>

--- backend/controllers/container.controller.js
+++ backend/controllers/container.controller.js
@@ -1,12 +1,9 @@
 import Container from "../models/container.model.js";
 
-export const containerByNumber = (req, res) => {
-  const { containerNumber } = req.params
-  const container = Container.findOne({ containerNumber})
+export const getContainers = (req, res) => {
+  Container.find()
     .then((result) => {
-      if (!result) {
-        return res.status(404).json({ TODO: `Error handling Container not found.` })
-      }
+
       return res.json(result)
     }).catch((err) => {
       return res.status(500).json({ TODO: `Error handling ${err}` })
