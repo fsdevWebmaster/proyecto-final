122f5e5dd9298fa7a571a0f31a8e6217de36a8f0
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-08-28 08:57:46 -0500

    48 backend create endpoint for new journey log (#71)
    * - Include journey log creation into the new journey creation.
    - Journey log creation implemented.
    - Response format added to journey model.
    - StepValue types bug fixed in journey log model.
    
    * - Added steps and status config to .env
    - New journey endpoint implemented.
    - New journey log endpoint implemented.
    - Models refactor.
    
    ---------
    
    Co-authored-by: fis48 <info@fidelsilva.com>

--- backend/routes/journey.routes.js
+++ backend/routes/journey.routes.js
@@ -1,10 +1,17 @@
 import express from "express";
 const journeyRouter = express.Router();
 import { userAuth } from "../middlewares/users.middleware.js";
-import { getInTransit, newContainer, newJourney } from "../controllers/journey.controller.js";
+import { 
+  getInTransit, 
+  newContainer, 
+  newJourney, 
+  createJourneyLog, 
+  updateJourneyLog 
+} from "../controllers/journey.controller.js";
 
 journeyRouter.post('/container', userAuth, newContainer)
 journeyRouter.post('/journey', userAuth, newJourney);
 journeyRouter.get('/in-transit', userAuth, getInTransit);
+journeyRouter.post('/journey-log', userAuth, createJourneyLog);
 
 export default journeyRouter;
