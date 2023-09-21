ee458e7fb93f7c94af96f7ff52b16e20ba697ee8
Fran Jiménez Q <37299077+FranJimenezQ@users.noreply.github.com>
2023-09-20 19:30:40 -0600

    update container endpoint created (#100)

--- backend/routes/container.routes.js
+++ backend/routes/container.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { userAuth } from "../middlewares/users.middleware.js";
-import { newContainer, getContainers, containerByNumber } from "../controllers/container.controller.js";
+import { newContainer, getContainers, containerByNumber, updateContainer } from "../controllers/container.controller.js";
 
 const containerRouter = express.Router();
 
 containerRouter.post('/container', userAuth, newContainer)
 containerRouter.get('/containers', userAuth, getContainers);
 containerRouter.get('/container/:containerNumber', userAuth, containerByNumber);
+
+containerRouter.patch('/container/:id', userAuth, updateContainer)
 
 export default containerRouter;
