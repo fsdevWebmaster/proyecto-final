52fe68e41d299337bb975a566b63f9a2d7ae3eba
fsdevWebmaster <119712167+fsdevWebmaster@users.noreply.github.com>
2023-09-22 15:55:35 -0500

    - Scale page implemented. (#126)
    - Minor fixes.
    Fixes #118
    
    Co-authored-by: fis48 <info@fidelsilva.com>

--- frontend/src/components/Tables/TableAction.tsx
+++ frontend/src/components/Tables/TableAction.tsx
@@ -1,12 +1,12 @@
 import { ReactNode, MouseEvent } from 'react';
 import { IconButton, Tooltip, Typography } from '@mui/material';
-import { Journey } from '@models/Journey/Journey';
+import { JourneyModel } from '@models/Journey/Journey';
 
 interface ITableActionProps {
   title: string;
   name?: string
-  journey: Journey;
-  clickHandler: (e: MouseEvent<HTMLElement>, name:string, journey:Journey) => void;
+  journey?: JourneyModel;
+  clickHandler: (e: MouseEvent<HTMLElement>, name:string, journey:JourneyModel) => void;
   visible: boolean;
   icon: ReactNode;
   iconText?: string;
@@ -19,7 +19,7 @@ interface ITableActionProps {
 export const TableAction = ({title, name, clickHandler, visible, icon, colors, iconText, journey}: ITableActionProps) => {
   return visible && (
     <Tooltip title={title} arrow 
-      onClick={(e: MouseEvent<HTMLElement>, name: string, journey: Journey) => clickHandler(e, name, journey)}>
+      onClick={(e: MouseEvent<HTMLElement>, name: string, journey: JourneyModel) => clickHandler(e, name, journey)}>
       <IconButton
         sx={{
           '&:hover': { background: colors.background },
