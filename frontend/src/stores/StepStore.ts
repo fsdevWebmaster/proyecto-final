7aac3abf7e1005c4af7a77d5f2e4720042356079
fis48 <info@fidelsilva.com>
2023-10-06 16:04:54 -0500

    Revision comments fixed.

--- frontend/src/stores/StepStore.ts
+++ frontend/src/stores/StepStore.ts
@@ -1,8 +1,6 @@
 import { makeAutoObservable } from "mobx";
 import { StepModel } from "@models/Step/Step";
 import { stepApi } from "@services/api/stepApi";
-import { JourneyModel } from "@models/Journey/Journey";
-import { AxiosResponse } from "axios";
 
 class StepStore {
   stepsList: StepModel[] = []
