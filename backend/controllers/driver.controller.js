086ce8dec8f081373436e137270b6fb8f2e52fd9
fis48 <info@fidelsilva.com>
2023-11-17 15:42:20 -0500

    fixes dev

--- backend/controllers/driver.controller.js
+++ backend/controllers/driver.controller.js
@@ -73,7 +73,7 @@ export const getDriver = (req, res, next) => {
     const { idDoc } = body;
 
     if (!idDoc) {
-      return next(new Error("Missing data"));
+      next(new Error("Missing data"));
     }
 
     Driver.find({ idDoc })
@@ -81,7 +81,7 @@ export const getDriver = (req, res, next) => {
         const driverFound = result[0];
 
         if(!driverFound) {
-          return next(new Error("Driver not found"))
+          next(new Error("Driver not found"))
         }
 
         // generate jwt
