be8851b4592a4b5230171118a752fb5c7174e314
Fran Jiménez Q <37299077+FranJimenezQ@users.noreply.github.com>
2023-11-18 16:55:44 -0600

    Ultimos ajustes en el form del driver (#191)

--- frontend/src/components/DriverRegistrationForm/DriverRegistrationForm.tsx
+++ frontend/src/components/DriverRegistrationForm/DriverRegistrationForm.tsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 import type { FC } from 'react'
-import { Formik } from "formik";
-
+import { Formik, FormikHelpers } from "formik";
+import React, { forwardRef, useImperativeHandle } from 'react';
 import {
   Button,
   CircularProgress,
@@ -13,129 +13,156 @@ import { useNavigate } from "react-router";
 import { useRefMounted } from "@hooks/useRefMounted"
 import { useTranslation } from "react-i18next";
 import { IContainerFormProps } from '@common/interfaces';
+import { Driver } from '@models/Driver/Driver';
+import { driverApi } from "@services/api/driverApi";
 
-export const DriverRegistrationForm: FC<IContainerFormProps> = ({modalAction}: IContainerFormProps) => {
+export type DriverRegistrationFormRef = {
+  resetForm: () => void;
+};
 
-  const isMountedRef = useRefMounted();
-  const { t } : { t: any } = useTranslation();
-  const navigate = useNavigate();
-  const theme = useTheme();
-  
-  const initValues = {
-      name: '',
-      identification: ''
-  }
+export const DriverRegistrationForm = forwardRef<DriverRegistrationFormRef, IContainerFormProps>(
+  ({ modalAction }, ref) => {
+    const isMountedRef = useRefMounted();
+    const { t } : { t: any } = useTranslation();
+    const navigate = useNavigate();
+    const theme = useTheme();
 
-  const handleBack = () => {
-    navigate('/')
-  }
 
-  const handleSubmit = async (values: any,
-    { setErrors, setStatus, setSubmitting }: any): Promise<void> => {
-      try {
+      const handleBack = () => {
+      navigate('/')
+      }
 
-        console.log("TODO: Post to /driver", values);
+      const handleSubmit = async (values: Driver, formikBag: FormikHelpers<Driver>) => {
+        try {
+        const response = await driverApi.newDriver(values)
 
-        if (isMountedRef.current) {
-          setStatus({ succes: true })
-          setSubmitting(false)
+        if (response.data && response.status === 200) {
+          formikBag.setStatus({ success: true });
+          formikBag.setSubmitting(false);
+          modalAction('success', 'Conductor registrado con éxito');
+        }else{
+          throw new Error(response.data.error || 'Error al registrar el conductor');
         }
       } catch (error: any) {
         console.error(error);
         if (isMountedRef.current) {
-          setStatus({ success: false })
-          setErrors({ submit: error.message })
-          setSubmitting(false)
+          formikBag.setStatus({ success: false, errorMessage: error.message });
+          formikBag.setSubmitting(false);
+          modalAction('error', 'Error al registrar el conductor');
+        }
         }
       }
-      modalAction();
+
+      const initValues: Driver = {
+        id: '',
+        name: '',
+        idDoc: '',
+        email: '',
+      }
+
+    return (
+      <Formik
+        initialValues={initValues}
+        validationSchema={Yup.object().shape({
+          name: Yup.string().max(255).required(t('The name field is required')),
+          idDoc: Yup.string().max(255).required(t('The identification field is required')),
+          email: Yup.string().max(255).required(t('The email field is required'))
+        })}
+        onSubmit={handleSubmit}
+      >
+         {(formikProps) => {
+          const {
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit, 
+            isSubmitting,
+            touched,
+            values,
+            resetForm
+          } = formikProps;
+          useImperativeHandle(ref, () => ({
+            resetForm
+          }));
+
+          return (
+            <form noValidate onSubmit={handleSubmit}>
+              <TextField
+                  error={Boolean(touched.name && errors.name)}
+                  fullWidth
+                  required
+                  margin="normal"
+                  autoFocus
+                  helperText={touched.name && errors.name}
+                  label={t('Name')}
+                  name='name'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="text"
+                  value={values.name}
+                  variant="outlined"
+                  />
+                  <TextField
+                  error={Boolean(touched.idDoc && errors.idDoc)}
+                  fullWidth
+                  required
+                  helperText={touched.idDoc && errors.idDoc}
+                  label={t('Identification')}
+                  name='idDoc'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type='text'
+                  value={values.idDoc}
+                  variant='outlined'
+                  />
+                  <TextField
+                  style={{marginTop: '10px'}}
+                  error={Boolean(touched.email && errors.email)}
+                  fullWidth
+                  required
+                  helperText={touched.email && errors.email}
+                  label={t('Email')}
+                  name='email'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type='text'
+                  value={values.email}
+                  variant='outlined'
+                  />
+
+                  <Button
+                  sx={{
+                  mt: 4,
+                  mb: 2
+                  }}
+                  style={{ background: theme.colors.primary.dark }}
+                  startIcon={isSubmitting ? <CircularProgress size="1rem"/> : null}
+                  disabled={isSubmitting}
+                  type='submit'
+                  size='large'
+                  variant='contained'
+                  >
+                  {t('Save')}
+                  </Button>
+                  <Button
+                  sx={{
+                  mt: 4,
+                  mb: 2
+                  }}
+                  style={{ background: theme.colors.secondary.dark, marginLeft: 12 }}
+                  startIcon={isSubmitting ? <CircularProgress size="1-rem"/> : null}
+                  disabled={isSubmitting}
+                  type='submit'
+                  size='large'
+                  variant='contained'
+                  onClick={handleBack}
+                  >
+                  {t('Go back to main page')}
+                  </Button>
+            </form>
+          );
+        }}
+      </Formik>
+    );
   }
-
-
-  return(
-    <Formik
-      initialValues={initValues}
-      validationSchema={Yup.object().shape({
-        name: Yup.string()
-          .max(255)
-          .required(t('The name field is required')),
-        identification: Yup.string()
-          .max(255)
-          .required(t('The identification field is required'))
-      })}
-      onSubmit={handleSubmit}
-    >
-      {({
-        errors,
-        handleBlur,
-        handleChange,
-        handleSubmit,
-        isSubmitting,
-        touched,
-        values
-      }): JSX.Element => (
-        <form noValidate onSubmit={handleSubmit}>
-          <TextField
-            error={Boolean(touched.name && errors.name)}
-            fullWidth
-            required
-            margin="normal"
-            autoFocus
-            helperText={touched.name && errors.name}
-            label={t('Name')}
-            name='name'
-            onBlur={handleBlur}
-            onChange={handleChange}
-            type="text"
-            value={values.name}
-            variant="outlined"
-          />
-          <TextField
-          error={Boolean(touched.identification && errors.identification)}
-          fullWidth
-          required
-          helperText={touched.identification && errors.identification}
-          label={t('Identification')}
-          name='identification'
-          onBlur={handleBlur}
-          onChange={handleChange}
-          type='text'
-          value={values.identification}
-          variant='outlined'
-          />
-
-          <Button
-            sx={{
-              mt: 4,
-              mb: 2
-            }}
-            style={{ background: theme.colors.primary.dark }}
-            startIcon={isSubmitting ? <CircularProgress size="1rem"/> : null}
-            disabled={isSubmitting}
-            type='submit'
-            size='large'
-            variant='contained'
-          >
-            {t('Save')}
-          </Button>
-          <Button
-            sx={{
-              mt: 4,
-              mb: 2
-            }}
-            style={{ background: theme.colors.secondary.dark, marginLeft: 12 }}
-            startIcon={isSubmitting ? <CircularProgress size="1-rem"/> : null}
-            disabled={isSubmitting}
-            type='submit'
-            size='large'
-            variant='contained'
-            onClick={handleBack}
-          >
-            {t('Go back to main page')}
-          </Button>
-        </form>
-      )}
-    </Formik>
-  )
-
-}
+);
